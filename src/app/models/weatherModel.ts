9ddb288a3e02b0218b77335042023fafc28e4490
malinabucur <malinabucur13@gmail.com>
2023-04-06 17:08:37 +0300

    Weather Model in order to have hourlyForecasts

--- src/app/models/weatherModel.ts
+++ src/app/models/weatherModel.ts
@@ -1,43 +1,45 @@
 export interface WeatherModel {
-    location: {
-      name: string;
-      region: string;
-      country: string;
-      lat: number;
-      lon: number;
-      tz_id: string;
-      localtime_epoch: number;
-      localtime: string;
+  location: {
+    name: string;
+    region: string;
+    country: string;
+    lat: number;
+    lon: number;
+    tz_id: string;
+    localtime_epoch: number;
+    localtime: string;
+  };
+  current: {
+    last_updated_epoch: number;
+    last_updated: string;
+    temp_c: number;
+    temp_f: number;
+    is_day: number;
+    condition: {
+      text: string;
+      icon: string;
+      code: number;
     };
-    current: {
-      last_updated_epoch: number;
-      last_updated: string;
-      temp_c: number;
-      temp_f: number;
-      is_day: number;
-      condition: {
-        text: string;
-        icon: string;
-        code: number;
+  };
+  forecast: {
+    forecastday: {
+      date: string;
+      day: {
+        avgtemp_c: number;
+        condition: {
+          icon: string;
+          text: string;
+        };
       };
-    };
-    forecast: {
-      forecastday: {
-        date: string;
-        day: {
-          avgtemp_c: number;
-          condition: {
-            icon: string;
-            text: string;
-          }
+      hour: {
+        time_epoch: number;
+        time: string;
+        temp_c: number;
+        condition:{
+          icon: string;
+          text: string;
         }
-        hour: {
-          temp_c: number;
-          condition:{
-            icon: string;
-            text: string;
-          }
-        }
-      }[]
-    };
-  }
+      }
+    }[];
+  };
+}
